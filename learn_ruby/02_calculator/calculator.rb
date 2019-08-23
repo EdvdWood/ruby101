c0ca3d1697c54f23b3afd92cbf2996b3a9adb21e
Sagarmatra <ejvdhout@vivaldi.net>
2019-08-23 17:03:17 +0200

    Finished calculator

--- learn_ruby/02_calculator/calculator.rb
+++ learn_ruby/02_calculator/calculator.rb
@@ -15,3 +15,27 @@ def sum array
     return output
 end
 
+def multiply *inputs
+    output = 1
+    i = 0
+    while i < inputs.length
+        output = output * inputs[i]
+        i = i + 1
+    end
+    return output
+end
+
+def power n1, n2
+    return n1**n2
+end
+
+def factorial num
+    output = 1
+    i = 1
+    while i <= num
+        output = output * i
+        i = i + 1
+    end
+    return output
+end
+
