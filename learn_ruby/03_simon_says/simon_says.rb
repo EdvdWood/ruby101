5496275e4e3d466b534b619cceb4ce1bba88439e
Sagarmatra <ejvdhout@vivaldi.net>
2019-08-24 23:16:48 +0200

    Finished Exercises

--- learn_ruby/03_simon_says/simon_says.rb
+++ learn_ruby/03_simon_says/simon_says.rb
@@ -1,1 +1,34 @@
 #write your code here
+
+def echo text
+    text
+end
+
+def shout text
+    text.upcase
+end
+
+def repeat text, number = 2
+    i = 0
+    textout = ""
+    while i < number
+        textout = textout + text + " "
+        i = i + 1
+    end
+    return textout[0..-2]
+end
+
+def start_of_word text, number = 1
+    return text[0..number-1]
+end
+
+def first_word text
+    return text.split[0]
+end
+
+def titleize text
+    fullcaps = text.split.collect{|e| e.capitalize}
+    fullcaps = fullcaps.map{|e| (e == "And" or e == "Over" or e =="The") ? e.downcase : e}
+    fullcaps[0] = fullcaps[0].capitalize
+    fullcaps.join(" ")
+end
--- learn_ruby/04_pig_latin/pig_latin.rb
+++ learn_ruby/04_pig_latin/pig_latin.rb
@@ -1,1 +1,31 @@
 #write your code here
+
+
+def translate string
+    pigsentence = ""
+    input = string.split(" ")
+    input = input.each do |word| 
+        i = 0
+        while i < word.length do
+            if (word[i] == "a" or word[i] == "e" or word[i] == "u" or word[i] == "i" or word[i] == "o") and word[i-1..i] != "qu"
+                if i == 0 
+                    word = word + "ay"
+                else
+                    letter = word[0..i-1]
+                    word = word[i..-1]
+                    word = word + letter + "ay"
+                end
+                pigsentence = pigsentence + word + " "
+                break
+            else
+                puts "failure"
+                i = i + 1
+                next
+            end
+        end
+    end
+    return pigsentence[0..-2]
+end
+
+
+translate "test"
